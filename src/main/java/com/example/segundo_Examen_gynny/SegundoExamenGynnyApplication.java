a11d36f93e520709e071ae59fb9c37d2b95f334d
agustinnavas <145386817+agustinnavas@users.noreply.github.com>
2023-11-27 15:28:33 +0100

    primer commit del examen

---
+++ src/main/java/com/example/segundo_Examen_gynny/SegundoExamenGynnyApplication.java
@@ -0,0 +1,13 @@
+package com.example.segundo_Examen_gynny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoExamenGynnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoExamenGynnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/segundo_Examen_gynny/SegundoExamenGynnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.segundo_Examen_gynny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoExamenGynnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
